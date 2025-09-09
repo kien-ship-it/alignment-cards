158355e62610a15fffea0bd1c8c756ca085a8844
kien-ship-it <ldkientzs@gmail.com>
2025-09-09 04:46:07 -0400

    Create prototype0.js

---
+++ prototype0.js
@@ -0,0 +1,32 @@
+export const cards = [
+    {
+        "name": "Reducio Ad Absurdum",
+        "definition": "IDN",
+        "failureMode": "deadass",
+        "example": "onon"
+    }, 
+    {
+        "name": "Modus Tolens",
+        "definition": "IDN",
+        "failureMode": "deadass",
+        "example": "onon"
+    },
+    {
+        "name": "Modus Tollendo Ponens",
+        "definition": "IDN",
+        "failureMode": "deadass",
+        "example": "onon"
+    },
+    {
+        "name": "Modus Ponens",
+        "definition": "IDN",
+        "failureMode": "deadass",
+        "example": "onon"
+    },
+    {
+        "name": "Begging the question",
+        "definition": "IDN",
+        "failureMode": "deadass",
+        "example": "onon"
+    },
+  ];
